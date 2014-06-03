f4a6bef686acc90dfccfb2dae3fcb3aff92822da
carbohydrate <kfletcher87@gmail.com>
2014-06-03 00:06:48 -0500

    borders work

--- ods.php
+++ ods.php
@@ -4,14 +4,21 @@ class ods {
         $this->content = '<?xml version="1.0" encoding="UTF-8"?><office:document-content xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:presentation="urn:oasis:names:tc:opendocument:xmlns:presentation:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:rpt="http://openoffice.org/2005/report" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:drawooo="http://openoffice.org/2010/draw" xmlns:calcext="urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" xmlns:formx="urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0" xmlns:css3t="http://www.w3.org/TR/css3-text/" office:version="1.2">';
 
     }
-    public function addCell($sheet, $row, $column, $value, $type, $width = 1) {
-        $this->sheets[$sheet]['rows'][$row][$column]['cellProp'] = array('cellType'=>$type, 'cellValue'=>$value, 'xml:id'=>$column);
+    public function addCell($sheet, $row, $column, $value, $type, $border = 0, $width = 1) {
+        $this->sheets[$sheet]['rows'][$row][$column]['cellProp'] = array('cellType'=>$type, 'cellValue'=>$value, 'xml:id'=>$column, 'cellBorder'=>$border);
+    }
+    private function addBorder($border) {
+        //ce1=all ce2=top ce3=right ce4=bottom ce5=left ce6=top,left ce7=top,right ce8=bottom,right ce9=bottom,left
+        return '<table:table-cell table:style-name="ce' . $border . '" ';
     }
 
     public function getContent() {
         $sheetArr = $this->sheets;
         $string = $this->content;
-        $string .= '<office:scripts/><office:font-face-decls></office:font-face-decls><office:automatic-styles></office:automatic-styles>';
+        $string .= '<office:scripts/><office:font-face-decls></office:font-face-decls>';
+        $string .= '<office:automatic-styles>';
+        $string .= '<style:style style:name="ce0" style:family="table-cell" style:parent-style-name="Default"><style:table-cell-properties fo:border="none"/></style:style><style:style style:name="ce1" style:family="table-cell" style:parent-style-name="Default"><style:table-cell-properties fo:border="0.06pt solid #000000"/></style:style><style:style style:name="ce2" style:family="table-cell" style:parent-style-name="Default"><style:table-cell-properties fo:border-bottom="none" fo:border-left="none" fo:border-right="none" fo:border-top="0.06pt solid #000000"/></style:style><style:style style:name="ce3" style:family="table-cell" style:parent-style-name="Default"><style:table-cell-properties fo:border-bottom="none" fo:border-left="none" fo:border-right="0.06pt solid #000000" fo:border-top="none"/></style:style><style:style style:name="ce4" style:family="table-cell" style:parent-style-name="Default"><style:table-cell-properties fo:border-bottom="0.06pt solid #000000" fo:border-left="none" fo:border-right="none0" fo:border-top="none"/></style:style><style:style style:name="ce5" style:family="table-cell" style:parent-style-name="Default"><style:table-cell-properties fo:border-bottom="none" fo:border-left="0.06pt solid #000000" fo:border-right="none" fo:border-top="none"/></style:style><style:style style:name="ce6" style:family="table-cell" style:parent-style-name="Default"><style:table-cell-properties fo:border-bottom="none" fo:border-left="0.06pt solid #000000" fo:border-right="none" fo:border-top="0.06pt solid #000000"/></style:style><style:style style:name="ce7" style:family="table-cell" style:parent-style-name="Default"><style:table-cell-properties fo:border-bottom="none" fo:border-left="none" fo:border-right="0.06pt solid #000000" fo:border-top="0.06pt solid #000000"/></style:style><style:style style:name="ce8" style:family="table-cell" style:parent-style-name="Default"><style:table-cell-properties fo:border-bottom="0.06pt solid #000000" fo:border-left="none" fo:border-right="0.06pt solid #000000" fo:border-top="none"/></style:style><style:style style:name="ce9" style:family="table-cell" style:parent-style-name="Default"><style:table-cell-properties fo:border-bottom="0.06pt solid #000000" fo:border-left="0.06pt solid #000000" fo:border-right="none" fo:border-top="none"/></style:style>';
+        $string .= '</office:automatic-styles>';
         //print_r($sheetArr);
         $string .= '<office:body><office:spreadsheet>';
         foreach ($sheetArr as $sheetIndex => $sheetContent) {
@@ -20,7 +27,7 @@ class ods {
             foreach ($sheetContent['rows'] as $rowIndex => $rowContent) {
                 $string .= '<table:table-row>';
                 foreach ($rowContent as $cellIndex => $a) {
-                    $string .= '<table:table-cell ';
+                    $string .= $this->addBorder($a['cellProp']['cellBorder']);
                     $string .= 'office:value-type ="' . $a['cellProp']['cellType'] . '">';
                     $string .= '<text:p>' . $a['cellProp']['cellValue'] . '</text:p>';
                     $string .= '</table:table-cell>';
@@ -84,10 +91,11 @@ class ods {
         $zip->addEmptyDir('Configurations2/statusbar');
         $zip->addEmptyDir('Configurations2/toolbar');
         $zip->close();
+        chmod($path, 0777);
     }
 }
 $obj = new ods;
-$obj->addCell(1,1,1,'test','string');
+$obj->addCell(1,1,1,'test','string', 4);
 $path = 'uploads/file.ods';
 $obj->exportOds($path);
 ?>
